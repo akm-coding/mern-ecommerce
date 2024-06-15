439bd9eafe61250a555e7820b20cd5b93a7435d6
Aung Kaung Myat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-06-15 21:28:15 +0700

    Set up for client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,34 @@
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme } from "./utils/Themes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
+
 function App() {
-  return <div>Hi</div>;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
